7834c57b31fb2ca4168539a75ff6cdee2a588437
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-02-24 16:33:03 +0100

    feat(back): add cors + socket.io

--- back/app.js
+++ back/app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var corsOptions = {
+  origin: 'http://streamchat.traefik.me',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
